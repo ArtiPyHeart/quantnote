0f19090cb66396c58c5be755af7d0c101573bb47
ArtiPyHeart <>
2020-04-12 23:16:10 +0800

    update AFML chapter 16.

--- snippets/advances_in_financial_machine_learning.py
+++ snippets/advances_in_financial_machine_learning.py
@@ -1281,3 +1281,435 @@ def batch(
         print(comb_[0], comb_[1], mean, std, mean / std)
         output1.append((comb_[0], comb_[1], mean, std, mean / std))
     return output1
+
+
+# -----------------------------------------------------------------------------
+# 14.1 deriving the timing of bets from a series of target positions
+# A bet takes place between flat positions or position flips
+df0 = tPos[tPos == 0].index
+df1 = tPos.shift(1)
+df1 = df1[df1 != 0].index
+bets = df0.intersection(df1)  # flattening
+df0 = tPos.iloc[1:] * tPos.iloc[:-1].values
+bets = bets.union(df0[df0 < 0].index).sort_values()  # tPos flips
+if tPos.index[-1] not in bets:
+    bets = bets.append(tPos.index[-1:])  # last bet
+
+# -----------------------------------------------------------------------------
+# 14.2 implementation of a holding period estimator
+def getHoldingPeriod(tPos):
+    # Derive avg holding period (in days) using avg entry time pairing algo
+    hp, tEntry = pd.DataFrame(columns=["dT", "w"]), 0.0
+    pDiff, tDiff = tPos.diff(), (tPos.index - tPos.index[0]) / np.timedelta64(1, "D")
+    for i in range(1, tPos.shape[0]):
+        if pDiff.iloc[i] * tPos.iloc[i - 1] >= 0:  # increased or unchanged
+            if tPos.iloc[i] != 0:
+                tEntry = (
+                    tEntry * tPos.iloc[i - 1] + tDiff[i] * pDiff.iloc[i]
+                ) / tPos.iloc[i]
+        else:  # decreased
+            if tPos.iloc[i] * tPos.iloc[i - 1] < 0:  # flip
+                hp.loc[tPos.index[i], ["dT", "w"]] = (
+                    tDiff[i] - tEntry,
+                    abs(tPos.iloc[i - 1]),
+                )
+                tEntry = tDiff[i]  # reset entry time
+            else:
+                hp.loc[tPos.index[i], ["dT", "w"]] = (
+                    tDiff[i] - tEntry,
+                    abs(pDiff.iloc[i]),
+                )
+    if hp["w"].sum() > 0:
+        hp = (hp["dT"] * hp["w"]).sum() / hp["w"].sum()
+    else:
+        hp = np.nan
+    return hp
+
+
+# -----------------------------------------------------------------------------
+# 14.3 algorithm for deriving HHI concentration
+rHHIPos = getHHI(ret[ret >= 0])  # concentration of positive returns per bet
+rHHINeg = getHHI(ret[ret < 0])  # concentration of negative returns per bet
+tHHI = getHHI(ret.groupby(pd.TimeGrouper(freq="M")).count())  # concentr. bets/month
+
+
+def getHHI(betRet):
+    if betRet.shape[0] <= 2:
+        return np.nan
+    wght = betRet / betRet.sum()
+    hhi = (wght ** 2).sum()
+    hhi = (hhi - betRet.shape[0] ** -1) / (1.0 - betRet.shape[0] ** -1)
+    return hhi
+
+
+# -----------------------------------------------------------------------------
+# deriving the sequence of DD and TuW
+def computeDD_TuW(series, dollars=False):
+    # compute series of drawdowns and the time under water associated with them
+    df0 = series.to_frame("pnl")
+    df0["hwm"] = series.expanding().max()
+    df1 = df0.groupby("hwm").min().reset_index()
+    df1.columns = ["hwm", "min"]
+    df1.index = df0["hwm"].drop_duplicates(keep="first").index  # time of hwm
+    df1 = df1[df1["hwm"] > df1["min"]]  # hwm followed by a drawdown
+    if dollars:
+        dd = df1["hwm"] - df1["min"]
+    else:
+        dd = 1 - df1["min"] / df1["hwm"]
+    tuw = ((df1.index[1:] - df1.index[:-1]) / np.timedelta64(1, "Y")).values  # in years
+    tuw = pd.Series(tuw, index=df1.index[:-1])
+    return dd, tuw
+
+
+# -----------------------------------------------------------------------------
+# 15.1 targeting a sharpe ratio as a function of the number of bets
+out, p = [], 0.55
+for i in range(1000000):
+    rnd = np.random.binomial(n=1, p=p)
+    x = 1 if rnd == 1 else -1
+    out.append(x)
+print(np.mean(out), np.std(out), np.mean(out) / np.std(out))
+
+# -----------------------------------------------------------------------------
+# 15.2 using the SymPy library for symbolic operations
+import sympy
+
+sympy.init_printing(use_unicode=False, wrap_line=False, no_global=True)
+p, u, d = sympy.symbols("p u d")
+m2 = p * u ** 2 + (1 - p) * d ** 2
+m1 = p * u + (1 - p) * d
+v = m2 - m1 ** 2
+sympy.factor(v)
+
+# -----------------------------------------------------------------------------
+# 15.3 computing the implied precision
+def binHR(sl, pt, freq, tSR):
+    """
+    Given a trading rule characterized by the parameters {sl,pt,freq},
+    what's the min precision p required to achieve a Sharpe ratio tSR?
+    1) Inputs
+    sl: stop loss threshold
+    pt: profit taking threshold
+    freq: number of bets per year
+    tSR: target annual Sharpe ratio
+    2) Output
+    p: the min precision rate p required to achieve tSR
+    """
+    a = (freq + tSR ** 2) * (pt - sl) ** 2
+    b = (2 * freq * sl - tSR ** 2 * (pt - sl)) * (pt - sl)
+    c = freq * sl ** 2
+    p = (-b + (b ** 2 - 4 * a * c) ** 0.5) / (2.0 * a)
+    return p
+
+
+# -----------------------------------------------------------------------------
+# 15.4 computing the implied betting frequency
+def binFreq(sl, pt, p, tSR):
+    """
+    Given a trading rule characterized by the parameters {sl,pt,freq},
+    what's the number of bets/year needed to achieve a Sharpe ratio
+    tSR with precision rate p?
+    Note: Equation with radicals, check for extraneous solution.
+    1) Inputs
+    sl: stop loss threshold
+    pt: profit taking threshold
+    p: precision rate p
+    tSR: target annual Sharpe ratio
+    2) Output
+    freq: number of bets per year needed
+    """
+    freq = (
+        (tSR * (pt - sl)) ** 2 * p * (1 - p) / ((pt - sl) * p + sl) ** 2
+    )  # possible extraneous
+    if not np.isclose(binHR(sl, pt, freq, p), tSR):
+        return
+    return freq
+
+
+# -----------------------------------------------------------------------------
+# 15.5 calculating the strategy risk in practice
+import scipy.stats as ss
+
+
+def mixGaussians(mu1, mu2, sigma1, sigma2, prob1, nObs):
+    # Random draws from a mixture of gaussians
+    ret1 = np.random.normal(mu1, sigma1, size=int(nObs * prob1))
+    ret2 = np.random.normal(mu2, sigma2, size=int(nObs) - ret1.shape[0])
+    ret = np.append(ret1, ret2, axis=0)
+    np.random.shuffle(ret)
+    return ret
+
+
+def probFailure(ret, freq, tSR):
+    # Derive probability that strategy may fail
+    rPos, rNeg = ret[ret > 0].mean(), ret[ret <= 0].mean()
+    p = ret[ret > 0].shape[0] / float(ret.shape[0])
+    thresP = binHR(rNeg, rPos, freq, tSR)
+    risk = ss.norm.cdf(thresP, p, p * (1 - p))  # approximation to bootstrap
+    return risk
+
+
+def main():
+    # 1) Parameters
+    mu1, mu2, sigma1, sigma2, prob1, nObs = 0.05, -0.1, 0.05, 0.1, 0.75, 2600
+    tSR, freq = 2.0, 260
+    # 2) Generate sample from mixture
+    ret = mixGaussians(mu1, mu2, sigma1, sigma2, prob1, nObs)
+    # 3) Compute prob failure
+    probF = probFailure(ret, freq, tSR)
+    print("Prob strategy will fail", probF)
+
+
+# -----------------------------------------------------------------------------
+# 16.1 tree clustering using scipy functionality
+import scipy.cluster.hierarchy as sch
+import numpy as np
+import pandas as pd
+
+cov, corr = x.cov(), x.corr()
+dist = ((1 - corr) / 2.0) ** 0.5  # distance matrix
+link = sch.linkage(dist, "single")  # linkage matrix
+
+# -----------------------------------------------------------------------------
+# 16.2 quasi-diagonalization
+def getQuasiDiag(link):
+    # Sort clustered items by distance
+    link = link.astype(int)
+    sortIx = pd.Series([link[-1, 0], link[-1, 1]])
+    numItems = link[-1, 3]  # number of original items
+    while sortIx.max() >= numItems:
+        sortIx.index = range(0, sortIx.shape[0] * 2, 2)  # make space
+        df0 = sortIx[sortIx >= numItems]  # find clusters
+        i = df0.index
+        j = df0.values - numItems
+        sortIx[i] = link[j, 0]  # item 1
+        df0 = pd.Series(link[j, 1], index=i + 1)
+        sortIx = sortIx.append(df0)  # item 2
+        sortIx = sortIx.sort_index()  # re-sort
+        sortIx.index = range(sortIx.shape[0])  # re-index
+    return sortIx.tolist()
+
+
+# -----------------------------------------------------------------------------
+# 16.3 recursive bisection
+def getRecBipart(cov, sortIx):
+    # Compute HRP alloc
+    w = pd.Series(1, index=sortIx)
+    cItems = [sortIx]  # initialize all items in one cluster
+    while len(cItems) > 0:
+        cItems = [
+            i[j:k]
+            for i in cItems
+            for j, k in ((0, len(i) / 2), (len(i) / 2, len(i)))
+            if len(i) > 1
+        ]  # bi-section
+        for i in range(0, len(cItems), 2):  # parse in pairs
+            cItems0 = cItems[i]  # cluster 1
+            cItems1 = cItems[i + 1]  # cluster 2
+            cVar0 = getClusterVar(cov, cItems0)
+            cVar1 = getClusterVar(cov, cItems1)
+            alpha = 1 - cVar0 / (cVar0 + cVar1)
+            w[cItems0] *= alpha  # weight 1
+            w[cItems1] *= 1 - alpha  # weight 2
+    return w
+
+
+# -----------------------------------------------------------------------------
+# 16.4 full implementation of the HRP algorithm
+import scipy.cluster.hierarchy as sch
+import random
+
+
+def getIVP(cov, **kargs):
+    # Compute the inverse-variance portfolio
+    ivp = 1.0 / np.diag(cov)
+    ivp /= ivp.sum()
+    return ivp
+
+
+def getClusterVar(cov, cItems):
+    # Compute variance per cluster
+    cov_ = cov.loc[cItems, cItems]  # matrix slice
+    w_ = getIVP(cov_).reshape(-1, 1)
+    cVar = np.dot(np.dot(w_.T, cov_), w_)[0, 0]
+    return cVar
+
+
+def getQuasiDiag(link):
+    # Sort clustered items by distance
+    link = link.astype(int)
+    sortIx = pd.Series([link[-1, 0], link[-1, 1]])
+    numItems = link[-1, 3]  # number of original items
+    while sortIx.max() >= numItems:
+        sortIx.index = range(0, sortIx.shape[0] * 2, 2)  # make space
+        df0 = sortIx[sortIx >= numItems]  # find clusters
+        i = df0.index
+        j = df0.values - numItems
+        sortIx[i] = link[j, 0]  # item 1
+        df0 = pd.Series(link[j, 1], index=i + 1)
+        sortIx = sortIx.append(df0)  # item 2
+        sortIx = sortIx.sort_index()  # re-sort
+        sortIx.index = range(sortIx.shape[0])  # re-index
+    return sortIx.tolist()
+
+
+def getRecBipart(cov, sortIx):
+    # Compute HRP alloc
+    w = pd.Series(1, index=sortIx)
+    cItems = [sortIx]  # initialize all items in one cluster
+    while len(cItems) > 0:
+        cItems = [
+            i[j:k]
+            for i in cItems
+            for j, k in ((0, len(i) / 2), (len(i) / 2, len(i)))
+            if len(i) > 1
+        ]  # bi-section
+    for i in range(0, len(cItems), 2):  # parse in pairs
+        cItems0 = cItems[i]  # cluster 1
+        cItems1 = cItems[i + 1]  # cluster 2
+        cVar0 = getClusterVar(cov, cItems0)
+        cVar1 = getClusterVar(cov, cItems1)
+        alpha = 1 - cVar0 / (cVar0 + cVar1)
+        w[cItems0] *= alpha  # weight 1
+        w[cItems1] *= 1 - alpha  # weight 2
+    return w
+
+
+def correlDist(corr):
+    # A distance matrix based on correlation, where 0<=d[i,j]<=1
+    # This is a proper distance metric
+    dist = ((1 - corr) / 2.0) ** 0.5  # distance matrix
+    return dist
+
+
+def plotCorrMatrix(path, corr, labels=None):
+    # Heatmap of the correlation matrix
+    if labels is None:
+        labels = []
+    plt.pcolor(corr)
+    plt.colorbar()
+    plt.yticks(np.arange(0.5, corr.shape[0] + 0.5), labels)
+    plt.xticks(np.arange(0.5, corr.shape[0] + 0.5), labels)
+    plt.savefig(path)
+    plt.clf()
+    plt.close()  # reset pylab
+
+
+def generateData(nObs, size0, size1, sigma1):
+    # Time series of correlated variables
+    # 1) generating some uncorrelated data
+    np.random.seed(seed=12345)
+    random.seed(12345)
+    x = np.random.normal(0, 1, size=(nObs, size0))  # each row is a variable
+    # 2) creating correlation between the variables
+    cols = [random.randint(0, size0 - 1) for i in range(size1)]
+    y = x[:, cols] + np.random.normal(0, sigma1, size=(nObs, len(cols)))
+    x = np.append(x, y, axis=1)
+    x = pd.DataFrame(x, columns=range(1, x.shape[1] + 1))
+    return x, cols
+
+
+def main():
+    # 1) Generate correlated data
+    nObs, size0, size1, sigma1 = 10000, 5, 5, 0.25
+    x, cols = generateData(nObs, size0, size1, sigma1)
+    print([(j + 1, size0 + i) for i, j in enumerate(cols, 1)])
+    cov, corr = x.cov(), x.corr()
+    # 2) compute and plot correl matrix
+    plotCorrMatrix("HRP3_corr0.png", corr, labels=corr.columns)
+    # 3) cluster
+    dist = correlDist(corr)
+    link = sch.linkage(dist, "single")
+    sortIx = getQuasiDiag(link)
+    sortIx = corr.index[sortIx].tolist()  # recover labels
+    df0 = corr.loc[sortIx, sortIx]  # reorder
+    plotCorrMatrix("HRP3_corr1.png", df0, labels=df0.columns)
+    # 4) Capital allocation
+    hrp = getRecBipart(cov, sortIx)
+    print(hrp)
+
+
+# -----------------------------------------------------------------------------
+# 16.5 monte carlo experiment on HRP out-of-sample performance
+from .cla import CLA
+
+
+def generateData(nObs, sLength, size0, size1, mu0, sigma0, sigma1F):
+    # Time series of correlated variables
+    # 1) generate random uncorrelated data
+    x = np.random.normal(mu0, sigma0, size=(nObs, size0))
+    # 2) create correlation between the variables
+    cols = [random.randint(0, size0 - 1) for i in range(size1)]
+    y = x[:, cols] + np.random.normal(0, sigma0 * sigma1F, size=(nObs, len(cols)))
+    x = np.append(x, y, axis=1)
+    # 3) add common random shock
+    point = np.random.randint(sLength, nObs - 1, size=2)
+    x[np.ix_(point, [cols[0], size0])] = np.array([[-0.5, -0.5], [2, 2]])
+    # 4) add specific random shock
+    point = np.random.randint(sLength, nObs - 1, size=2)
+    x[point, cols[-1]] = np.array([-0.5, 2])
+    return x, cols
+
+
+def getHRP(cov, corr):
+    # Construct a hierarchical portfolio
+    corr, cov = pd.DataFrame(corr), pd.DataFrame(cov)
+    dist = correlDist(corr)
+    link = sch.linkage(dist, "single")
+    sortIx = getQuasiDiag(link)
+    sortIx = corr.index[sortIx].tolist()  # recover labels
+    hrp = getRecBipart(cov, sortIx)
+    return hrp.sort_index()
+
+
+def getCLA(cov, **kargs):
+    # Compute CLA's minimum variance portfolio
+    mean = np.arange(cov.shape[0]).reshape(-1, 1)  # Not used by C portf
+    lB = np.zeros(mean.shape)
+    uB = np.ones(mean.shape)
+    cla = CLA(mean, cov, lB, uB)
+    cla.solve()
+    return cla.w[-1].flatten()
+
+
+def hrpMC(
+    numIters=1e4,
+    nObs=520,
+    size0=5,
+    size1=5,
+    mu0=0,
+    sigma0=1e-2,
+    sigma1F=0.25,
+    sLength=260,
+    rebal=22,
+):
+    # Monte Carlo experiment on HRP
+    methods = [getIVP, getHRP, getCLA]
+    stats, numIter = {i.__name__: pd.Series() for i in methods}, 0
+    pointers = range(sLength, nObs, rebal)
+    while numIter < numIters:
+        print(numIter)
+        # 1) Prepare data for one experiment
+        x, cols = generateData(nObs, sLength, size0, size1, mu0, sigma0, sigma1F)
+        r = {i.__name__: pd.Series() for i in methods}
+        # 2) Compute portfolios in-sample
+        for pointer in pointers:
+            x_ = x[pointer - sLength : pointer]
+            cov_, corr_ = np.cov(x_, rowvar=0), np.corrcoef(x_, rowvar=0)
+            # 3) Compute performance out-of-sample
+            x_ = x[pointer : pointer + rebal]
+        for func in methods:
+            w_ = func(cov=cov_, corr=corr_)  # callback
+            r_ = pd.Series(np.dot(x_, w_))
+            r[func.__name__] = r[func.__name__].append(r_)
+        # 4) Evaluate and store results
+        for func in methods:
+            r_ = r[func.__name__].reset_index(drop=True)
+            p_ = (1 + r_).cumprod()
+            stats[func.__name__].loc[numIter] = p_.iloc[-1] - 1
+        numIter += 1
+    # 5) Report results
+    stats = pd.DataFrame.from_dict(stats, orient="columns")
+    stats.to_csv("stats.csv")
+    df0, df1 = stats.std(), stats.var()
+    print(pd.concat([df0, df1, df1 / df1["getHRP"] - 1], axis=1))
