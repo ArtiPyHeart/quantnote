a113b344b377baadd4130e0d1c363d958017b6ed
ArtiPyHeart <>
2020-04-13 22:21:42 +0800

    finish AFML chapter 21. All code sample finished.

--- snippets/advances_in_financial_machine_learning.py
+++ snippets/advances_in_financial_machine_learning.py
@@ -2201,17 +2201,6 @@ def mpJobList(
 
 # -----------------------------------------------------------------------------
 # 20.14 principal components for a subset of the columns
-pcs = mpJobList(
-    getPCs,
-    ("molecules", fileNames),
-    numThreads=24,
-    mpBatches=1,
-    path=path,
-    eVec=eVec,
-    redux=pd.DataFrame.add,
-)
-
-
 def getPCs(path, molecules, eVec):
     # get principal components by loading one file at a time
     pcs = None
@@ -2223,3 +2212,150 @@ def getPCs(path, molecules, eVec):
             pcs += np.dot(df0.values, eVec.loc[df0.columns].values)
     pcs = pd.DataFrame(pcs, index=df0.index, columns=eVec.columns)
     return pcs
+
+
+pcs = mpJobList(
+    getPCs,
+    ("molecules", fileNames),
+    numThreads=24,
+    mpBatches=1,
+    path=path,
+    eVec=eVec,
+    redux=pd.DataFrame.add,
+)
+
+# -----------------------------------------------------------------------------
+# 21.1 partitions of k objects into n slots
+from itertools import combinations_with_replacement
+
+
+def pigeonHole(k, n):
+    # Pigeonhole problem (organize k objects in n slots)
+    for j in combinations_with_replacement(range(n), k):
+        r = [0] * n
+        for i in j:
+            r[i] += 1
+    yield r
+
+
+# -----------------------------------------------------------------------------
+# 21.2 set omega of all vectors associated with all partitions
+def getAllWeights(k, n):
+    # 1) Generate partitions
+    parts, w = pigeonHole(k, n), None
+    # 2) Go through partitions
+    for part_ in parts:
+        w_ = np.array(part_) / float(k)  # abs(weight) vector
+        for prod_ in product([-1, 1], repeat=n):  # add sign
+            w_signed_ = (w_ * prod_).reshape(-1, 1)
+            if w is None:
+                w = w_signed_.copy()
+            else:
+                w = np.append(w, w_signed_, axis=1)
+    return w
+
+
+# -----------------------------------------------------------------------------
+# 21.3 evaluating all trajectories
+def evalTCosts(w, params):
+    # Compute t-costs of a particular trajectory
+    tcost = np.zeros(w.shape[1])
+    w_ = np.zeros(shape=w.shape[0])
+    for i in range(tcost.shape[0]):
+        c_ = params[i]["c"]
+        tcost[i] = (c_ * abs(w[:, i] - w_) ** 0.5).sum()
+        w_ = w[:, i].copy()
+    return tcost
+
+
+def evalSR(params, w, tcost):
+    # Evaluate SR over multiple horizons
+    mean, cov = 0, 0
+    for h in range(w.shape[1]):
+        params_ = params[h]
+        mean += np.dot(w[:, h].T, params_["mean"])[0] - tcost[h]
+        cov += np.dot(w[:, h].T, np.dot(params_["cov"], w[:, h]))
+    sr = mean / cov ** 0.5
+    return sr
+
+
+def dynOptPort(params, k=None):
+    # Dynamic optimal portfolio
+    # 1) Generate partitions
+    if k is None:
+        k = params[0]["mean"].shape[0]
+    n = params[0]["mean"].shape[0]
+    w_all, sr = getAllWeights(k, n), None
+    # 2) Generate trajectories as cartesian products
+    for prod_ in product(w_all.T, repeat=len(params)):
+        w_ = np.array(prod_).T  # concatenate product into a trajectory
+        tcost_ = evalTCosts(w_, params)
+        sr_ = evalSR(params, w_, tcost_)  # evaluate trajectory
+        if sr is None or sr < sr_:  # store trajectory if better
+            sr, w = sr_, w_.copy()
+    return w
+
+
+# -----------------------------------------------------------------------------
+# 21.4 produce a random matrix of a given rank
+def rndMatWithRank(nSamples, nCols, rank, sigma=0, homNoise=True):
+    # Produce random matrix X with given rank
+    rng = np.random.RandomState()
+    U, _, _ = np.linalg.svd(rng.randn(nCols, nCols))
+    x = np.dot(rng.randn(nSamples, rank), U[:, :rank].T)
+    if homNoise:
+        x += sigma * rng.randn(nSamples, nCols)  # Adding homoscedastic noise
+    else:
+        sigmas = sigma * (rng.rand(nCols) + 0.5)  # Adding heteroscedastic noise
+        x += rng.randn(nSamples, nCols) * sigmas
+    return x
+
+
+# -----------------------------------------------------------------------------
+# 21.5 generate the problem's parameters
+def genMean(size):
+    # Generate a random vector of means
+    rMean = np.random.normal(size=(size, 1))
+    return rMean
+
+
+# 1) Parameters
+size, horizon = 3, 2
+params = []
+for h in range(horizon):
+    x = rndMatWithRank(1000, 3, 3, 0.0)
+    mean_, cov_ = genMean(size), np.cov(x, rowvar=False)
+    c_ = np.random.uniform(size=cov_.shape[0]) * np.diag(cov_) ** 0.5
+    params.append({"mean": mean_, "cov": cov_, "c": c_})
+
+# -----------------------------------------------------------------------------
+# 21.6 compute and evaluate the static solution
+def statOptPortf(cov, a):
+    # Static optimal porftolio
+    # Solution to the "unconstrained" portfolio optimization problem
+    cov_inv = np.linalg.inv(cov)
+    w = np.dot(cov_inv, a)
+    w /= np.dot(np.dot(a.T, cov_inv), a)  # np.dot(w.T,a)==1
+    w /= abs(w).sum()  # re-scale for full investment
+    return w
+
+
+# 2) Static optimal portfolios
+w_stat = None
+for params_ in params:
+    w_ = statOptPortf(cov=params_["cov"], a=params_["mean"])
+    if w_stat is None:
+        w_stat = w_.copy()
+    else:
+        w_stat = np.append(w_stat, w_, axis=1)
+tcost_stat = evalTCosts(w_stat, params)
+sr_stat = evalSR(params, w_stat, tcost_stat)
+print("static SR:", sr_stat)
+
+# -----------------------------------------------------------------------------
+# 21.7 compute and evaluate the dynamic solution
+# 3) Dynamic optimal portfolios
+w_dyn = dynOptPort(params)
+tcost_dyn = evalTCosts(w_dyn, params)
+sr_dyn = evalSR(params, w_dyn, tcost_dyn)
+print("dynamic SR:", sr_dyn)
